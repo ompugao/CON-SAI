06460f85bcd6832dc87fa87039028e5fecd36f50
Shohei Fujii <fujii.shohei@gmail.com>
2018-05-03 01:27:03 +0900

    hotfix!

--- decision_making/scripts/game_evaluator.py
+++ decision_making/scripts/game_evaluator.py
@@ -53,17 +53,19 @@ class Admiral(object):
     def select_play(self, current_situation):
         if self.current_play.name == "PlayDummy" or \
                 (callable(self.finish_keeping_play) and self.finish_keeping_play()):
-            self.reset()
-            if current_situation is 'IN_PLAY':
-                self.current_play, self.finish_keeping_play = self.select_play_inplay()
-            elif current_situation is 'BALL_IN_OUR_DEFENCE':
-                self.current_play, self.finish_keeping_play = self.select_play_ball_in_our_defence()
-            elif current_situation is 'BALL_IN_THEIR_DEFENCE':
-                self.current_play, self.finish_keeping_play = self.select_play_ball_in_their_defence()
-            elif current_situation is 'OUR_INDIRECT':
-                self.current_play, self.finish_keeping_play = self.select_play_our_indirect()
-            elif current_situation is 'OUR_DIRECT':
-                self.current_play, self.finish_keeping_play = self.select_play_our_direct()
+            if self.last_situation != current_situation:
+                self.reset()
+                if current_situation is 'IN_PLAY':
+                    self.current_play, self.finish_keeping_play = self.select_play_inplay()
+                elif current_situation is 'BALL_IN_OUR_DEFENCE':
+                    self.current_play, self.finish_keeping_play = self.select_play_ball_in_our_defence()
+                elif current_situation is 'BALL_IN_THEIR_DEFENCE':
+                    self.current_play, self.finish_keeping_play = self.select_play_ball_in_their_defence()
+                elif current_situation is 'OUR_INDIRECT':
+                    self.current_play, self.finish_keeping_play = self.select_play_our_indirect()
+                elif current_situation is 'OUR_DIRECT':
+                    self.current_play, self.finish_keeping_play = self.select_play_our_direct()
+        self.last_situation = current_situation
 
         return self.current_play
 
